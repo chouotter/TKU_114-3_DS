4991c16245458d672e3e190dedb0bae2b7c6036e
chouotter <easonchou15217@gmail.com>
2026-07-04 00:59:32 +0800

    0702

---
+++ 0702/ProfileCard.java
@@ -0,0 +1,18 @@
+public class ProfileCard {
+    public static void main(String[] args) {
+        System.out.println("=== Profile ===");
+        // 這一行會輸出 Profile
+        String name = "Amy";
+        //宣告名字
+        String Department = "Information Management";
+        //宣告科系
+        String course ="Programming";
+        //宣告喜歡的課程
+
+        System.out.println("Name:" + name);
+        System.out.println("Department:" + Department);
+        System.out.println("Favorite course:" + course);
+        /*輸出結果
+         */
+    }
+}
